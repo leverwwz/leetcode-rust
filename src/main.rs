46a49dc3c777daa79c0bc6d37b8fafac8fb8bdb8
leverwwz <1395592289@qq.com>
2022-02-03 11:53:21 +0800

    add main func

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
 struct Solution {}
 
 impl Solution {
-    pub fn quick_sort(nums: &mut Vec<i32>, l: usize, r: usize) {
-        if l > r {
-            return;
-        }
-        let mut i = l;
-        let mut j = r;
-        while i < j {
-            while i < j && nums[j] >= nums[l] { j -= 1; };
-            while i < j && nums[i] < nums[l] { i += 1; }
-            nums.swap(i, j);
-        }
-        nums.swap(l, i);
-        quick_sort(nums, l, i - 1);
-        quick_sort(nums, i + 1, r);
-    }
-
-
     //offer 10 https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
     pub fn num_ways(n: i32) -> i32 {
         let mut a = 0;
@@ -34,6 +17,11 @@ impl Solution {
     }
 }
 
+fn main_num_ways() {
+    let r = Solution::num_ways(4);
+    println!("res is {}", r);
+}
+
 fn quick_sort(nums: &mut Vec<i32>, l: usize, r: usize) {
     if l >= r {
         return;
@@ -46,19 +34,18 @@ fn quick_sort(nums: &mut Vec<i32>, l: usize, r: usize) {
         nums.swap(i, j);
     }
     nums.swap(l, i);
-    if i>1{
+    if i > 1 {
         quick_sort(nums, l, i - 1);
     }
 
     quick_sort(nums, i + 1, r);
 }
 
-fn main() {
-    // let r = Solution::num_ways(4);
-    // println!("res is {}", r);
-
+fn main_quick_sort() {
     let mut v = vec![4, 2, 8, 5, 6, 1];
     let right = v.len() - 1;
     quick_sort(&mut v, 0, right);
     println!("sorted v is {:?}", v);
 }
+
+fn main() {}
